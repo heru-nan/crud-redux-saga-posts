c5dc9844e4292683714a68023f102b2a8473e4d5
Hernan <hrwgallardo@gmail.com>
2020-05-02 18:43:29 -0400

    mock api rest server

---
+++ client/mock-api/create.mock.db.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const state = require('../state.js');
+
+const {posts} = state;
+const data = JSON.stringify({posts});
+const filepath = path.join(__dirname, "db.json");
+
+fs.writeFile(filepath, data, function(err){
+    err? console.log(err): console.log("db created successfully");
+})
--- client/state.js
+++ client/state.js
@@ -28,4 +28,4 @@ const state = {
     ]
 }
 
-export default state;
+module.exports = state;
